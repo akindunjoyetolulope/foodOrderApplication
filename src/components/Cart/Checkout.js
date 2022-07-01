c2a530031acf7613c1881962903d37a8e1cd79bd
Tolu <tweetbro99@gmail.com>
2022-07-01 13:46:54 +0100

    userData to database

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,11 +1,16 @@
 import { useRef, useState } from 'react';
 
+
 import classes from './Checkout.module.css';
+
+
 
 const isEmpty = (value) => value.trim() === '';
 const isFiveChars = (value) => value.trim().length === 5;
 
 const Checkout = (props) => {
+
+
   const [formInputsValidity, setFormInputsValidity] = useState({
     name: true,
     street: true,
@@ -49,6 +54,8 @@ const Checkout = (props) => {
     }
 
     // Submit cart data
+    const userData = {enteredName, enteredStreet, enteredPostalCode, enteredCity}
+    props.submitOrderHandler(userData)
   };
 
   const nameControlClasses = `${classes.control} ${
